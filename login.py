1cfb2ff3441e722b41dff7f5a394e3be83b6e467
unknown <LucasFeliciano02@outlook.com>
2021-12-17 18:28:46 -0300

    INT: Terminado script de login do backend.

---
+++ login.py
@@ -0,0 +1,12 @@
+while True:
+    usuario = input('Nome do Usuário: ')
+    senha = input('Senha do Usuário: ')
+    usuario_bd = 'Lucas'
+    senha_bd = '123456'
+
+    if usuario == usuario_bd and senha == senha_bd:
+        print('Entrando')
+        break
+    else:
+        print('Usuário ou senha invalidos\nTente novamente ')
+        continue
